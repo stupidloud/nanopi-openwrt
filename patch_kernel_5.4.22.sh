c801e26f43c2e4b220437063ff3163d25791e0ab
klever1988 <56048681+klever1988@users.noreply.github.com>
2020-03-14 05:37:43 +0800

    Create patch_kernel_5.4.22.sh

---
+++ patch_kernel_5.4.22.sh
@@ -0,0 +1,13 @@
+cd friendlywrt-rk3328
+cd kernel/ && git apply ../../add_fullconenat.diff && cd ../
+git clone https://github.com/openwrt/openwrt && cd openwrt/
+git checkout 4e0c54bc5bc8381e031af5147b66b4dadeecc626
+rm target/linux/generic/pending-5.4/403-mtd-hook-mtdsplit-to-Kbuild.patch
+rm target/linux/generic/hack-5.4/700-swconfig_switch_drivers.patch
+./scripts/patch-kernel.sh ../kernel target/linux/generic/backport-5.4
+./scripts/patch-kernel.sh ../kernel target/linux/generic/pending-5.4
+./scripts/patch-kernel.sh ../kernel target/linux/generic/hack-5.4
+cd ../
+wget https://github.com/torvalds/linux/raw/master/scripts/kconfig/merge_config.sh && chmod +x merge_config.sh
+grep -i '_NETFILTER_\|FLOW' ../.config.override > .config.override
+./merge_config.sh -m .config.override kernel/arch/arm64/configs/nanopi-r2_linux_defconfig && mv .config kernel/arch/arm64/configs/nanopi-r2_linux_defconfig
