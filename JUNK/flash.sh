6b7b5f6677e52a0753ee71f212e6b42260c15c3d
klever <k@k.com>
2020-03-16 00:24:51 +0800

    add and remove some packages

--- JUNK/flash.sh
+++ JUNK/flash.sh
@@ -1,11 +1,16 @@
 #!/bin/ash
 
 if ! type "pv" > /dev/null; then
-opkg update ; opkg install pv
+	opkg update ; opkg install pv
+	if ! type "pv" > /dev/null; then
+		echo 'pv安装失败，退出...'
+		exit 1
+	fi
 fi
+
 if [ -f /tmp/upload/rom.img.gz ]; then
-pv /tmp/upload/rom.img.gz | gunzip -dc > /dev/mmcblk0
-echo 1 > /proc/sys/kernel/sysrq
-echo b > /proc/sysrq-trigger
+	pv /tmp/upload/rom.img.gz | gunzip -dc > /dev/mmcblk0
+	echo 1 > /proc/sys/kernel/sysrq
+	echo b > /proc/sysrq-trigger
 fi
 
