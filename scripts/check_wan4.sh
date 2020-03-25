5d39c384de7bf2ea8e3ba8ee456b5d34479d62c4
klever1988 <56048681+klever1988@users.noreply.github.com>
2020-03-26 07:31:44 +0800

    Add files via upload

--- scripts/check_wan4.sh
+++ scripts/check_wan4.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-source /lib/functions/network.sh
-network_find_wan NET_IF
-network_get_gateway NET_GW "${NET_IF}"
-
-r=`ping -c1 "${NET_GW}"`
-r=`echo $r | grep Permitted`
 while true; do
-    if [[ "$r" != "" ]] ;then
-        /etc/init.d/firewall restart
-    fi
+    source /lib/functions/network.sh
+    network_find_wan NET_IF
+    network_get_gateway NET_GW "${NET_IF}"
+
+    r=`ping -c1 "${NET_GW}" 2>&1`
+    #r=`echo $r | grep permitted`
+    case $r in
+        *permitted* ) /etc/init.d/firewall restart ;;
+    esac
+    #    /etc/init.d/firewall restart
     sleep 2
 done
