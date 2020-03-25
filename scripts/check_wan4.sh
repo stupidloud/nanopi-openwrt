9e4dc8d6a44bd43740b464f04c2258ad79e12431
klever1988 <56048681+klever1988@users.noreply.github.com>
2020-03-26 07:33:49 +0800

    Update check_wan4.sh

--- scripts/check_wan4.sh
+++ scripts/check_wan4.sh
@@ -6,10 +6,8 @@ while true; do
     network_get_gateway NET_GW "${NET_IF}"
 
     r=`ping -c1 "${NET_GW}" 2>&1`
-    #r=`echo $r | grep permitted`
     case $r in
         *permitted* ) /etc/init.d/firewall restart ;;
     esac
-    #    /etc/init.d/firewall restart
     sleep 2
 done
