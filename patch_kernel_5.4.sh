de0bd66bcb2488f650e32d12159f9f08c7cff982
klever1988 <56048681+klever1988@users.noreply.github.com>
2020-04-24 10:22:33 +0800

    Update patch_kernel_5.4.sh

--- patch_kernel_5.4.sh
+++ patch_kernel_5.4.sh
@@ -6,8 +6,9 @@ git apply RK3328-enable-1512mhz-opp.patch
 cd ../
 git clone https://github.com/openwrt/openwrt && cd openwrt/
 #git checkout 4e0c54bc5bc8381e031af5147b66b4dadeecc626
-rm target/linux/generic/pending-5.4/403-mtd-hook-mtdsplit-to-Kbuild.patch
-rm target/linux/generic/hack-5.4/700-swconfig_switch_drivers.patch
+#rm target/linux/generic/pending-5.4/403-mtd-hook-mtdsplit-to-Kbuild.patch
+#rm target/linux/generic/hack-5.4/700-swconfig_switch_drivers.patch
+cp -a ./target/linux/generic/files/* ../rk3328/kernel/
 ./scripts/patch-kernel.sh ../kernel target/linux/generic/backport-5.4
 ./scripts/patch-kernel.sh ../kernel target/linux/generic/pending-5.4
 ./scripts/patch-kernel.sh ../kernel target/linux/generic/hack-5.4
