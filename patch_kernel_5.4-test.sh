eba6230702273c9cd1bc5ba1da533d476c29bada
klever1988 <56048681+klever1988@users.noreply.github.com>
2020-08-06 06:07:32 +0800

    Update patch_kernel_5.4-test.sh

--- patch_kernel_5.4-test.sh
+++ patch_kernel_5.4-test.sh
@@ -1,8 +1,8 @@
 cd friendlywrt-rk3328
 cd kernel/
 git apply ../../add_fullconenat.diff
-wget https://github.com/QiuSimons/R2S-OpenWrt/raw/master/PATCH/new/main/998-rockchip-enable-i2c0-on-NanoPi-R2S.patch
-git apply RK3328-enable-1512mhz-opp.patch
+wget https://raw.githubusercontent.com/QiuSimons/R2S-OpenWrt/master/PATCH/new/main/999-unlock-1608mhz-rk3328.patch
+git apply 999-unlock-1608mhz-rk3328.patch
 cd ../
 git clone https://github.com/coolsnowwolf/lede && cd lede/
 git checkout 707e6ed1a19db0a12f553cafbd5851d3591420af
