ceb34642ff4c406a424425386c893cf94e79aac6
root <root@DESKTOP-0PQNDF5.localdomain>
2020-09-05 15:51:43 +0800

    add theme infinityfreedom

---
+++ 3_prepare_packages.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/NateLol/luci-app-oled
+git clone https://github.com/destan19/OpenAppFilter.git
+git clone https://github.com/rufengsuixing/luci-app-adguardhome
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
