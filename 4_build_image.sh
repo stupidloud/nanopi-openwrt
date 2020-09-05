cc1285886e0053132361e0c6f58b3a998872f131
root <root@DESKTOP-0PQNDF5.localdomain>
2020-09-06 02:32:47 +0800

    fix dependency conflicts fix rewrite scripts

--- 4_build_image.sh
+++ 4_build_image.sh
@@ -8,7 +8,7 @@ echo -e '\nCONFIG_TCP_CONG_BBR=m' >> kernel/arch/arm/configs/sunxi_defconfig
 sed -i 's/set -eu/set -u/' scripts/mk-friendlywrt.sh
 ./build.sh $2
 LOOP_DEVICE=$(losetup -f)
-losetup -o 100663296 ${LOOP_DEVICE} friendlywrt-*/out/*.img
-rm -rf /mnt/friendlywrt-tmp && mkdir -p /mnt/friendlywrt-tmp
-mount ${LOOP_DEVICE} /mnt/friendlywrt-tmp && chown -R root:root /mnt/friendlywrt-tmp && umount /mnt/friendlywrt-tmp
-losetup -d ${LOOP_DEVICE}
+sudo losetup -o 100663296 ${LOOP_DEVICE} out/*.img
+sudo rm -rf /mnt/friendlywrt-tmp && sudo mkdir -p /mnt/friendlywrt-tmp
+sudo mount ${LOOP_DEVICE} /mnt/friendlywrt-tmp && chown -R root:root /mnt/friendlywrt-tmp && sudo umount /mnt/friendlywrt-tmp
+sudo losetup -d ${LOOP_DEVICE}
