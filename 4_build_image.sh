d2b71938b05434221e0c2e046671be547a1a215d
root <root@DESKTOP-0PQNDF5.localdomain>
2020-09-06 12:38:07 +0800

    fix image size re-order scripts

--- 4_build_image.sh
+++ 4_build_image.sh
@@ -3,10 +3,13 @@ cd friendlywrt
 ./scripts/feeds update -a && ./scripts/feeds install -a
 sed -i '/STAMP_BUILT/d' feeds/packages/utils/runc/Makefile feeds/packages/utils/containerd/Makefile #fix compile error
 cd ..
+
 echo -e '\nCONFIG_TCP_CONG_ADVANCED=y' >> kernel/arch/arm/configs/sunxi_defconfig
 echo -e '\nCONFIG_TCP_CONG_BBR=m' >> kernel/arch/arm/configs/sunxi_defconfig
 sed -i 's/set -eu/set -u/' scripts/mk-friendlywrt.sh
+sed -i 's/640/1000/' scripts/sd-fuse/mk-sd-image.sh
 ./build.sh $2
+
 LOOP_DEVICE=$(losetup -f)
 sudo losetup -o 100663296 ${LOOP_DEVICE} out/*.img
 sudo rm -rf /mnt/friendlywrt-tmp && sudo mkdir -p /mnt/friendlywrt-tmp
