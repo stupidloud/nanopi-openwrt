3987b322a8bcd7d69c3ac4cffa4d0aef7bbf27d5
root <root@DESKTOP-0PQNDF5.localdomain>
2020-09-07 16:36:17 +0800

    fix chown

--- 4_build_image.sh
+++ 4_build_image.sh
@@ -13,5 +13,5 @@ sed -i 's/640/1000/' scripts/sd-fuse/mk-sd-image.sh
 LOOP_DEVICE=$(losetup -f)
 sudo losetup -o 100663296 ${LOOP_DEVICE} out/*.img
 sudo rm -rf /mnt/friendlywrt-tmp && sudo mkdir -p /mnt/friendlywrt-tmp
-sudo mount ${LOOP_DEVICE} /mnt/friendlywrt-tmp && chown -R root:root /mnt/friendlywrt-tmp && sudo umount /mnt/friendlywrt-tmp
+sudo mount ${LOOP_DEVICE} /mnt/friendlywrt-tmp && sudo chown -R root:root /mnt/friendlywrt-tmp && sudo umount /mnt/friendlywrt-tmp
 sudo losetup -d ${LOOP_DEVICE}
