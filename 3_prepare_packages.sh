b7d6e975fc8326a91b852262a7b21298a951506f
root <root@DESKTOP-0PQNDF5.localdomain>
2020-09-13 11:41:56 +0800

    maintain package selection

--- 3_prepare_packages.sh
+++ 3_prepare_packages.sh
@@ -1,11 +1,13 @@
 if [[ `pwd` == *"r2s"* ]]; then
   git clone https://github.com/NateLol/luci-app-oled
 fi
-git clone https://github.com/destan19/OpenAppFilter.git
+git clone https://github.com/destan19/OpenAppFilter
 git clone https://github.com/rufengsuixing/luci-app-adguardhome
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge 
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
+git clone https://github.com/pexcn/openwrt-chinadns-ng chinadns-ng
+git clone https://github.com/WuSiYu/luci-app-chinadns-ng
 
 #git clone https://github.com/jerrykuku/lua-maxminddb.git
 #git clone https://github.com/jerrykuku/luci-app-vssr.git
