e995752641463b8ae38866501fa5f806a84acca4
root <root@DESKTOP-0PQNDF5.localdomain>
2020-09-21 02:44:24 +0800

    show system info

--- 3_prepare_packages.sh
+++ 3_prepare_packages.sh
@@ -4,7 +4,7 @@ fi
 [ ! -d luci-app-adguardhome ] && git clone https://github.com/rufengsuixing/luci-app-adguardhome
 [ ! -d luci-app-mentohust ] && git clone https://github.com/BoringCat/luci-app-mentohust
 [ ! -d mentohust ] && svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/mentohust
-rm -rf luci-theme-argon/ && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+rm -rf luci-theme-argon*/ ../lean/luci-theme-argon/ && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 rm -rf luci-theme-edge/ && git clone -b 18.06 https://github.com/garypang13/luci-theme-edge
 rm -rf luci-theme-infinityfreedom/ && git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
 rm -rf luci-app-chinadns-ng/ && git clone https://github.com/WuSiYu/luci-app-chinadns-ng
