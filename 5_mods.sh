afa6010b66636d8f4d957cdf73da2a770828ebe1
root <root@DESKTOP-0PQNDF5.localdomain>
2020-10-06 02:52:18 +0800

    add wireguard.ko to autoload

--- 5_mods.sh
+++ 5_mods.sh
@@ -4,4 +4,5 @@ sed -i '/exit/i\find /etc/rc.d/ -name *docker* -delete' package/lean/default-set
 sed -i 's/option fullcone\t1/option fullcone\t0/' package/network/config/firewall/files/firewall.config
 sed -i '/find.*wlan.*wc/a\WIFI_NUM=0' package/base-files/files/root/setup.sh
 sed -i '/8.8.8.8/d' package/base-files/files/root/setup.sh
+echo 'wireguard' > base-files/etc/modules.d/10_wireguard
 echo -e '\nDYC Build\n' >> package/base-files/files/etc/banner
