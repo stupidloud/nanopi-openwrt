ad77797fa7e46d50f54cb4e3f3481c881a6e1a45
root <root@DESKTOP-0PQNDF5.localdomain>
2020-10-17 00:05:45 +0800

    package upgrade: libpcap

--- add_passwall.sh
+++ add_passwall.sh
@@ -1,6 +1,5 @@
-cd friendlywrt-rk3328/friendlywrt
 echo -e '\nsrc-git lienol https://github.com/xiaorouji/openwrt-package' >> feeds.conf.default
-./scripts/feeds update -a
+./scripts/feeds update lienol
 rm -rf feeds/lienol/lienol/ipt2socks
 rm -rf feeds/lienol/lienol/shadowsocksr-libev
 rm -rf feeds/lienol/lienol/pdnsd-alt
@@ -11,4 +10,4 @@ rm -rf package/lean/luci-app-kodexplorer
 rm -rf package/lean/luci-app-pppoe-relay
 rm -rf package/lean/luci-app-pptp-server
 rm -rf package/lean/luci-app-v2ray-server
-./scripts/feeds install -a
+./scripts/feeds install -p lienol
