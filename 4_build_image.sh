3b2ccd73b894fd8956ada12aea3b942b99ce9228
qzhome2102 <qzhome2102@hotmail.com>
2020-10-23 15:43:59 +0800

    10-23
    10-23

--- 4_build_image.sh
+++ 4_build_image.sh
@@ -1,11 +1,11 @@
 cd $1
 cd friendlywrt
-./scripts/feeds update -a && ./scripts/feeds install -a
 sed -i '/STAMP_BUILT/d' feeds/packages/utils/runc/Makefile feeds/packages/utils/containerd/Makefile #fix compile error
 cd ..
 
 echo -e '\nCONFIG_TCP_CONG_ADVANCED=y' >> kernel/arch/arm/configs/sunxi_defconfig
 echo -e '\nCONFIG_TCP_CONG_BBR=m' >> kernel/arch/arm/configs/sunxi_defconfig
+sed -i '/feeds/d' scripts/mk-friendlywrt.sh
 sed -i 's/set -eu/set -u/' scripts/mk-friendlywrt.sh
 sed -i 's/640/1000/' scripts/sd-fuse/mk-sd-image.sh
 ./build.sh $2
