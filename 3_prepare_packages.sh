9f34b0bb14119820f032df8b4d0c56ad226c3492
qzhome2102 <qzhome2102@hotmail.com>
2020-10-23 15:44:58 +0800

    7
    7

--- 3_prepare_packages.sh
+++ 3_prepare_packages.sh
@@ -1,8 +1,11 @@
+<<<<<<< Updated upstream
 ./scripts/feeds update -a && ./scripts/feeds install -a
 rm -rf feeds/packages/libs/libcap && svn co https://github.com/openwrt/packages/trunk/libs/libcap feeds/packages/libs/libcap
 sed -i '/enable-jsonc/i\\t--disable-cloud \\' feeds/packages/admin/netdata/Makefile 
 
 cd package/lean/
+=======
+>>>>>>> Stashed changes
 if [[ `pwd` == *"rk3328"* ]]; then
   rm -rf luci-app-oled/ && git clone https://github.com/NateLol/luci-app-oled
 fi
@@ -14,8 +17,11 @@ rm -rf luci-theme-edge/ && git clone -b 18.06 https://github.com/garypang13/luci
 rm -rf luci-theme-infinityfreedom/ && git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
 rm -rf luci-app-chinadns-ng/ && git clone https://github.com/WuSiYu/luci-app-chinadns-ng
 rm -rf openwrt-chinadns-ng/ && git clone https://github.com/pexcn/openwrt-chinadns-ng
+<<<<<<< Updated upstream
 cd ../../
 
+=======
+>>>>>>> Stashed changes
 
 #git clone https://github.com/jerrykuku/lua-maxminddb.git
 #git clone https://github.com/jerrykuku/luci-app-vssr.git
