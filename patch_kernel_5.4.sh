b2d851c85af763c8405e324b0e374688ba9b2d4f
root <root@DESKTOP-0PQNDF5.localdomain>
2020-11-18 19:27:42 +0800

    update

--- patch_kernel_5.4.sh
+++ patch_kernel_5.4.sh
@@ -5,7 +5,7 @@ wget https://github.com/armbian/build/raw/master/patch/kernel/rockchip64-dev/RK3
 git apply RK3328-enable-1512mhz-opp.patch
 cd ../
 git clone https://github.com/openwrt/openwrt && cd openwrt/
-git checkout 05b8e84362b8455dec6db9b862826f21e8f24341
+git checkout a47279154e08d54df05fa8bf45fe935ebf0df5da
 #rm target/linux/generic/pending-5.4/403-mtd-hook-mtdsplit-to-Kbuild.patch
 #rm target/linux/generic/hack-5.4/700-swconfig_switch_drivers.patch
 cp -a ./target/linux/generic/files/* ../kernel/
