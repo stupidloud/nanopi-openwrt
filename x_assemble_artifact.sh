df4410c1cb5546849c72219c8edb8aef7e8cbf76
biliwala <72666349+biliwala@users.noreply.github.com>
2020-12-24 13:49:38 +0800

    specify gz file to assemble

--- x_assemble_artifact.sh
+++ x_assemble_artifact.sh
@@ -1,6 +1,6 @@
 gzip friendlywrt-*/out/*.img
 rm -rf ./artifact/ && mkdir -p ./artifact/
-mv friendlywrt-*/out/*img* ./artifact/
+mv friendlywrt-*/out/*img.gz ./artifact/
 cp friendlywrt-*/friendlywrt/.config ./artifact/
 cd ./artifact/
 md5sum *img* > md5sum.txt
