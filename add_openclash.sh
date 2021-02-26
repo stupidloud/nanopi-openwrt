b22518d4d8d4a4a8b980fcf9b5eb4bbbc0f6c879
johndoe <klever1988@protonmail.com>
2021-02-27 06:20:25 +0800

    Buddhist-style update

--- add_openclash.sh
+++
@@ -1,8 +0,0 @@
-cd ../../
-git clone -b master https://github.com/vernesong/OpenClash.git 
-mv OpenClash/luci-app-openclash friendlywrt-rk3328/friendlywrt/package
-cd friendlywrt-rk3328/friendlywrt/package/base-files/files
-mkdir -p etc/openclash/core && cd etc/openclash/core
-curl -L https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-armv8.tar.gz | tar zxf -
-chmod +x clash
-cd ../../../../../../
