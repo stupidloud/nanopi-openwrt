f6267e0448d2d8e6daf6731f1e5382799e59bcf4
johndoe <klever1988@protonmail.com>
2021-02-27 06:20:25 +0800

    Buddhist-style update

--- fix_ownership.sh
+++
@@ -1,5 +0,0 @@
-LOOP_DEVICE=$(losetup -f)
-losetup -o 100663296 ${LOOP_DEVICE} friendlywrt-*/out/*.img
-rm -rf /mnt/friendlywrt-tmp && mkdir -p /mnt/friendlywrt-tmp
-mount ${LOOP_DEVICE} /mnt/friendlywrt-tmp && chown -R root:root /mnt/friendlywrt-tmp && umount /mnt/friendlywrt-tmp
-losetup -d ${LOOP_DEVICE}
