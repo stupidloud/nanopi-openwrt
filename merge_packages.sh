301646295449c1a2ed99568fc48815caf3a0571c
John Doe <klever1988@protonmail.com>
2021-03-05 13:02:18 +0800

    package: fix cifsd

--- merge_packages.sh
+++ merge_packages.sh
@@ -1,3 +1,5 @@
 git clone --depth=1 https://github.com/destan19/OpenAppFilter.git && rm -r package/OpenAppFilter && mv OpenAppFilter package/
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git && rm -r package/lean/luci-theme-argon && mv luci-theme-argon package/lean/
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-cifsd && rm -r package/lean/luci-app-ksmbd && mv luci-app-cifsd package/lean/
+svn co https://github.com/coolsnowwolf/packages/trunk/kernel/ksmbd && rm -r package/feeds/packages/ksmbd && mv ksmbd package/feeds/packages/
+svn co https://github.com/coolsnowwolf/packages/trunk/net/ksmbd-tools && rm -r package/feeds/packages/ksmbd-tools && mv ksmbd-tools package/feeds/packages/
