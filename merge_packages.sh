1bfbe031945afe404f965a8989b549a601689a0a
John Doe <klever1988@protonmail.com>
2021-03-05 15:34:24 +0800

    service: add pwm fan service(https://github.com/friendlyarm/friendlywrt/commit/e1fb88ff29bcf634c875b94a9026565c7780149f)

--- merge_packages.sh
+++ merge_packages.sh
@@ -3,3 +3,15 @@ git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git &
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-cifsd && rm -r package/lean/luci-app-ksmbd && mv luci-app-cifsd package/lean/
 svn co https://github.com/coolsnowwolf/packages/trunk/kernel/ksmbd && rm -r package/feeds/packages/ksmbd && mv ksmbd package/feeds/packages/
 svn co https://github.com/coolsnowwolf/packages/trunk/net/ksmbd-tools && rm -r package/feeds/packages/ksmbd-tools && mv ksmbd-tools package/feeds/packages/
+
+mkdir -p target/linux/rockchip/armv8/base-files/usr/bin &&\
+wget https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3328/base-files/usr/bin/start-rk3328-pwm-fan.sh -P target/linux/rockchip/armv8/base-files/usr/bin &&\
+chmod +x target/linux/rockchip/armv8/base-files/usr/bin/start-rk3328-pwm-fan.sh
+
+mkdir -p target/linux/rockchip/armv8/base-files/etc/init.d &&\
+wget https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3328/base-files/etc/init.d/fa-rk3328-pwmfan -P target/linux/rockchip/armv8/base-files/etc/init.d &&\
+chmod +x target/linux/rockchip/armv8/base-files/etc/init.d/fa-rk3328-pwmfan
+
+mkdir -p target/linux/rockchip/armv8/base-files/etc/rc.d &&\
+ln -s ../init.d/fa-rk3328-pwmfan target/linux/rockchip/armv8/base-files/etc/rc.d/S96fa-rk3328-pwmfan
+chmod +x target/linux/rockchip/armv8/base-files/etc/rc.d/S96fa-rk3328-pwmfan
