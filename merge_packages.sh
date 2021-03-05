90aee879f265e480c8e856cf35c611acfea82887
John Doe <klever1988@protonmail.com>
2021-03-05 16:32:23 +0800

    image: enlarge parttition size of root

--- merge_packages.sh
+++ merge_packages.sh
@@ -8,14 +8,13 @@ svn co https://github.com/coolsnowwolf/packages/trunk/net/ksmbd-tools && rm -r p
 
 if [ $DEVICE = 'r2s' ]; then
 mkdir -p target/linux/rockchip/armv8/base-files/usr/bin &&\
-wget https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3328/base-files/usr/bin/start-rk3328-pwm-fan.sh -P target/linux/rockchip/armv8/base-files/usr/bin &&\
+wget https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3328/base-files/usr/bin/start-rk3328-pwm-fan.sh -qNP target/linux/rockchip/armv8/base-files/usr/bin &&\
 chmod +x target/linux/rockchip/armv8/base-files/usr/bin/start-rk3328-pwm-fan.sh
 mkdir -p target/linux/rockchip/armv8/base-files/etc/init.d &&\
-wget https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3328/base-files/etc/init.d/fa-rk3328-pwmfan -P target/linux/rockchip/armv8/base-files/etc/init.d &&\
+wget https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3328/base-files/etc/init.d/fa-rk3328-pwmfan -qNP target/linux/rockchip/armv8/base-files/etc/init.d &&\
 chmod +x target/linux/rockchip/armv8/base-files/etc/init.d/fa-rk3328-pwmfan
 mkdir -p target/linux/rockchip/armv8/base-files/etc/rc.d &&\
-ln -s ../init.d/fa-rk3328-pwmfan target/linux/rockchip/armv8/base-files/etc/rc.d/S96fa-rk3328-pwmfan
-chmod +x target/linux/rockchip/armv8/base-files/etc/rc.d/S96fa-rk3328-pwmfan
+ln -sf ../init.d/fa-rk3328-pwmfan target/linux/rockchip/armv8/base-files/etc/rc.d/S96fa-rk3328-pwmfan
 
 git clone --depth=1 https://github.com/NateLol/luci-app-oled && mv luci-app-oled package/
 fi
