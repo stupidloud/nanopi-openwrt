bfdb61a6dc3c813184759ad210e37f04856f9610
John Doe <klever1988@protonmail.com>
2021-04-13 03:48:03 +0800

    Build: allow dns port PREROUTING

--- merge_files.sh
+++ merge_files.sh
@@ -10,3 +10,6 @@ fi
 chmod 600 files/etc/dropbear/*
 eval `cat .config | grep \" | head -n 10`
 . files/etc/opkg/distfeeds.conf | tee files/etc/opkg/distfeeds.conf
+
+echo "iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53" >> files/etc/firewall.user
+echo "iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53" >> files/etc/firewall.user
