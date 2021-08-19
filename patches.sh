d59ce37629da2c3327e578e1b12c0957d63bc916
Topsy Chen <62056970+thomaswcy@users.noreply.github.com>
2021-08-19 14:50:40 +0800

    Update patches.sh

--- patches.sh
+++ patches.sh
@@ -24,6 +24,15 @@ if [ $DEVICE = 'r2s' ]; then
     echo -e "\toption maxfreq0 '1512000'\n" >> package/feeds/luci/luci-app-cpufreq/root/etc/config/cpufreq
 fi
 
+if [ $DEVICE = 'r2c' ]; then
+    sed -i "s/enable '0'/enable '1'/" `find feeds/ -type f -name oled | grep config`
+    sed -i 's/1400000/1450000/' target/linux/rockchip/patches-5.4/991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
+    truncate -s-1 package/feeds/luci/luci-app-cpufreq/root/etc/config/cpufreq
+    echo -e "\toption governor0 'schedutil'" >> package/feeds/luci/luci-app-cpufreq/root/etc/config/cpufreq
+    echo -e "\toption minfreq0 '816000'" >> package/feeds/luci/luci-app-cpufreq/root/etc/config/cpufreq
+    echo -e "\toption maxfreq0 '1512000'\n" >> package/feeds/luci/luci-app-cpufreq/root/etc/config/cpufreq
+fi
+
 if [ $DEVICE = 'r4s' ]; then
     wget https://github.com/immortalwrt/immortalwrt/commit/6c3f6d2686679173b95495c47d861db1f41729dd.patch
     sed -i 's/package\/ctcgfw\/r8168\///g' 6c3f6d2686679173b95495c47d861db1f41729dd.patch
