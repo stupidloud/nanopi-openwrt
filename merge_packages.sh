2535f352ecd4065beebe9def87e402fba9b4cabb
John Doe <klever1988@protonmail.com>
2021-08-27 09:38:32 +0800

    package: remove luci-app-cifsd replacement

--- merge_packages.sh
+++ merge_packages.sh
@@ -24,9 +24,6 @@ function merge_feed(){
 
 merge_feed nas "https://github.com/linkease/nas-packages.git;master"
 merge_package https://github.com/project-lede/luci-app-godproxy
-merge_package https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-cifsd luci-app-ksmbd
-merge_package https://github.com/coolsnowwolf/packages/trunk/kernel/ksmbd
-merge_package https://github.com/coolsnowwolf/packages/trunk/net/ksmbd-tools
 merge_package https://github.com/Beginner-Go/luci-app-tencentddns
 merge_package https://github.com/sundaqiang/openwrt-packages/trunk/luci-app-services-wolplus
 merge_package https://github.com/coolsnowwolf/lede/trunk/package/lean/ntfs3-mount
