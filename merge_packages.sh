b55e20b65cbb1a7686f8ad00951a65deb97fc051
John Doe <klever1988@protonmail.com>
2021-09-02 10:42:43 +0800

    Package: add uugamebooster, aliyun ddns script, autoreboot

--- merge_packages.sh
+++ merge_packages.sh
@@ -28,3 +28,4 @@ merge_package https://github.com/Beginner-Go/luci-app-tencentddns
 merge_package https://github.com/sundaqiang/openwrt-packages/trunk/luci-app-services-wolplus
 merge_package https://github.com/coolsnowwolf/lede/trunk/package/lean/ntfs3-mount
 merge_package https://github.com/coolsnowwolf/lede/trunk/package/lean/ntfs3
+merge_package https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-uugamebooster
