ec5d4ef46d79d83c681b2f49d4b3615adc708545
John Doe <klever1988@protonmail.com>
2021-10-10 23:40:54 +0800

    Package: add mosdns

--- scripts/merge_packages.sh
+++ scripts/merge_packages.sh
@@ -24,6 +24,7 @@ function merge_feed(){
 
 merge_feed nas "https://github.com/linkease/nas-packages;master"
 merge_feed nas_luci "https://github.com/linkease/nas-packages-luci;main"
+merge_feed mosdns "https://github.com/klever1988/openwrt-mos;master"
 merge_package https://github.com/project-lede/luci-app-godproxy
 merge_package https://github.com/Beginner-Go/luci-app-tencentddns
 merge_package https://github.com/sundaqiang/openwrt-packages/trunk/luci-app-services-wolplus
