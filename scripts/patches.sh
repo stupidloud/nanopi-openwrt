4c28062bfdf979be74e5316cb0a12bf958d38fd9
John Doe <klever1988@protonmail.com>
2021-10-22 17:19:02 +0800

    fix r4s

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -25,6 +25,7 @@ sed -i 's/5.0/1.0/' .ccache/ccache.conf || true
 if [ $BRANCH == 'master' ]; then
 
   echo '# CONFIG_UCLAMP_TASK is not set' >> target/linux/sunxi/config-5.4
+  sed -i '/LINUX_5_4/d' package/kernel/r8168/Makefile
 
   # fix po path for snapshot
   find package/ -follow -type d -path '*/po/zh-cn' | xargs dirname | xargs -n1 -i sh -c "rm -f {}/zh_Hans; ln -sf zh-cn {}/zh_Hans"
