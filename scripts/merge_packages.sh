e3002132fec64b8370d211ffdae845a8796120c8
John Doe <klever1988@protonmail.com>
2021-10-22 18:07:07 +0800

    fix

--- scripts/merge_packages.sh
+++ scripts/merge_packages.sh
@@ -14,7 +14,7 @@ function merge_package(){
     mv $pn package/custom/
 }
 function drop_package(){
-    find package/ -follow -name $pn -not -path "package/custom/*" | xargs -rt rm -r
+    find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -r
 }
 function merge_feed(){
     if [ ! -d "feed/$1" ]; then
