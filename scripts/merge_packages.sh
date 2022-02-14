f5ec09a32b8582617c87e60b499fd5c2c8598f06
John Doe <klever1988@protonmail.com>
2022-02-14 18:58:23 +0800

    Fix

--- scripts/merge_packages.sh
+++ scripts/merge_packages.sh
@@ -36,7 +36,7 @@ merge_package https://github.com/sundaqiang/openwrt-packages/trunk/luci-app-wolp
 merge_package https://github.com/coolsnowwolf/lede/trunk/package/lean/automount
 merge_package https://github.com/coolsnowwolf/lede/trunk/package/lean/ntfs3-mount
 merge_package https://github.com/coolsnowwolf/lede/trunk/package/lean/ntfs3-oot
-merge_package https://github.com/coolsnowwolf/lede/trunk/package/lean/uugamebooster
+merge_package https://github.com/coolsnowwolf/packages/trunk/net/uugamebooster
 merge_package https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-uugamebooster
 merge_package https://github.com/kuoruan/openwrt-frp frp
 merge_package https://github.com/kuoruan/luci-app-frpc
