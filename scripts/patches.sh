77d4d5bdfd62b47fada59d5c46242ce77dc0515d
John Doe <klever1988@protonmail.com>
2022-04-04 00:07:26 +0800

    Package: revert fixing openclash

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -10,7 +10,6 @@ sed -i "/\/etc\/coremark\.sh/d" `find package/ -follow -type f -path '*/coremark
 sed -i 's/192.168.1.1/192.168.2.1/' package/base-files/files/bin/config_generate
 sed -i 's/=1/=0/g' package/kernel/linux/files/sysctl-br-netfilter.conf
 
-sed -i '/DEPENDS/ s/$/ +libcap-bin/' `find package/ -follow -type f -path '*/luci-app-openclash/Makefile'`
 sed -i '/DEPENDS+/ s/$/ +wsdd2/' `find package/ -follow -type f -path '*/ksmbd-tools/Makefile'`
 sed -i '/DEPENDS/ s/$/ +frpc/' `find package/ -follow -type f -path '*/luci-app-frpc/Makefile'`
 
