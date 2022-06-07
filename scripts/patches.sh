c5f2bde6a7af182ba85ac9d88c66de415dabcf58
John Doe <klever1988@protonmail.com>
2022-06-08 03:59:44 +0800

    fix: (coolsnowwolf/lede#9483)

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -95,6 +95,9 @@ if [[ $DEVICE == 'r1s' ]]; then
   merge_package https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/emortal/autocore
 elif [[ $DEVICE == 'r2s' || $DEVICE == 'r2c' || $DEVICE == 'r1p' || $DEVICE == 'r1p-lts' ]]; then
   sed -i 's/5.15/5.4/g' target/linux/rockchip/Makefile
+elif [[ $DEVICE == 'r4s' || $DEVICE == 'r1p' || $DEVICE == 'r1p-lts' ]]; then
+  line_number_CONFIG_CRYPTO_LIB_BLAKE2S=$[`grep -n 'CONFIG_CRYPTO_LIB_BLAKE2S' package/kernel/linux/modules/crypto.mk | cut -d: -f 1`+1]
+  sed -i $line_number_CONFIG_CRYPTO_LIB_BLAKE2S' s/HIDDEN:=1/DEPENDS:=@(LINUX_5_4)/' package/kernel/linux/modules/crypto.mk
 fi
 
 # ...
