365a2f46d8533763dff8d85e6134a1146dd65203
John Doe <klever1988@protonmail.com>
2022-06-09 00:10:25 +0800

    test

--- 5_mods.sh
+++ 5_mods.sh
@@ -2,5 +2,4 @@ sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/argon"' 
 sed -i '/exit/i\chown -R root:root /usr/share/netdata/web' package/lean/default-settings/files/zzz-default-settings
 sed -i '/exit/i\find /etc/rc.d/ -name *docker* -delete' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/option fullcone\t1/option fullcone\t0/' package/network/config/firewall/files/firewall.config
-echo 'wireguard' > package/base-files/files/etc/modules.d/10_wireguard
 echo -e '\nDYC Build\n' >> package/base-files/files/etc/banner
