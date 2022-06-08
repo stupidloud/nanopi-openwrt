5819251fbb504b2a67ff9f3b44bd56d17a00c5b2
John Doe <klever1988@protonmail.com>
2022-06-08 22:30:05 +0800

    test

--- 2_clone_source.sh
+++ 2_clone_source.sh
@@ -7,8 +7,4 @@ repo sync -c --no-tags --no-clone-bundle -j8
 cd friendlywrt/ && git fetch --unshallow
 git checkout `git branch -va | grep remotes/m | awk '{print $3}' | awk -F\/ '{print $2}'`
 
-[ -f target/linux/rockchip-rk3328/config-4.14 ] && sed -i '/CONFIG_CGROUPS/a\CONFIG_CGROUP_PERF=y' target/linux/rockchip-rk3328/config-4.14
-[ -f target/linux/allwinner-h5/config-4.14 ] &&    sed -i '/CONFIG_CGROUPS/a\CONFIG_CGROUP_PERF=y' target/linux/allwinner-h5/config-4.14
-git add . && git commit -m 'reset'
-
 cd ../ && find device/ -name distfeeds.conf -delete
