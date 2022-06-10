0f55eb14f70bfc0a82504c362bfd8d5b2d12b6b5
John Doe <klever1988@protonmail.com>
2022-06-10 22:23:12 +0800

    use 5.4

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -99,11 +99,8 @@ if [[ $DEVICE == 'r1s' ]]; then
   merge_package https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/emortal/autocore
 fi
 
-if [[ $DEVICE == 'r2s' || $DEVICE == 'r2c' || $DEVICE == 'r1p' || $DEVICE == 'r1p-lts' ]]; then
-  sed -i 's/5.10/5.4/g' target/linux/rockchip/Makefile
-fi
-
 if [[ $DEVICE == 'r4s' || $DEVICE == 'r2s' || $DEVICE == 'r2c' || $DEVICE == 'r1p' || $DEVICE == 'r1p-lts' ]]; then
+  sed -i 's/5.10/5.4/g' target/linux/rockchip/Makefile
   line_number_CONFIG_CRYPTO_LIB_BLAKE2S=$[`grep -n 'CONFIG_CRYPTO_LIB_BLAKE2S' package/kernel/linux/modules/crypto.mk | cut -d: -f 1`+1]
   sed -i $line_number_CONFIG_CRYPTO_LIB_BLAKE2S' s/HIDDEN:=1/DEPENDS:=@(LINUX_5_4||LINUX_5_10)/' package/kernel/linux/modules/crypto.mk
   echo 'kmod-wireguard' >> `ls staging_dir/target-*/pkginfo/linux.default.install`
