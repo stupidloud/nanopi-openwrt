4763f0fcf6425ce7c37fe6c7ed8e271bc5bc500d
John Doe <klever1988@protonmail.com>
2023-07-23 00:34:21 +0800

    fix

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -104,7 +104,7 @@ if [[ $DEVICE == 'r1s' ]]; then
   git log --grep r1s -i | grep '^commit ' | head -n -2 | cut -d' ' -f2 | tac | xargs git show | sed '0,/UENV/s//ATF/' > r1s.diff
   git show 632c4c91e7640a354dc421fa324fd705b734252d 7fb1b00f5f6214bf7a29d3781d260a7e7c8547c9 >> r1s.diff
   cd ~/lede && chmod +x target/linux/sunxi/base-files/etc/board.d/* && git apply ~/immortalwrt/r1s.diff
-  sed -i 's/kmod-rtl8189es//' target/linux/sunxi/image/cortexa53.mk
+  sed -i 's/kmod-rtl8189es//;s/wpad-basic-openssl/wpad-basic-wolfssl/' target/linux/sunxi/image/cortexa53.mk
   sed -i 's/arm-trusted-firmware-sunxi-a64/trusted-firmware-a-sunxi-a64/' package/boot/uboot-sunxi/Makefile
   merge_package https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/emortal/autocore
 fi
@@ -117,7 +117,7 @@ fi
 
 # fix for r1s-h3
 if [[ $DEVICE == 'r1s-h3' ]]; then
-  sed -i 's/kmod-leds-gpio//;s/wpad-basic-wolfssl/wpad-basic-openssl/;' target/linux/sunxi/image/cortexa7.mk
+  sed -i 's/kmod-leds-gpio//' target/linux/sunxi/image/cortexa7.mk
 fi
 
 case $DEVICE in
