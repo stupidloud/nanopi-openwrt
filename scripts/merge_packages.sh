1f3dc07c9a3aefc326c5d7dda912ff3def93dfe5
John Doe <klever1988@protonmail.com>
2023-12-07 13:47:32 +0800

    fix

--- scripts/merge_packages.sh
+++ scripts/merge_packages.sh
@@ -1,10 +1,10 @@
 function merge_package(){
     repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
-    find package/ -follow -name $3 -not -path "package/custom/*" | xargs -rt rm -rf
+    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
+    find package/ -follow -name $pkg -not -path "package/custom/*" | xargs -rt rm -rf
     git clone --depth=1 --single-branch $1
-    mv $2 $3
+    mv $2 package/custom/
     rm -rf $repo
-    mv $3 package/custom/
 }
 function drop_package(){
     find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
