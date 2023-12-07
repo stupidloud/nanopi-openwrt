96cc2b66018b2014639ef0f9e2105057b2ee26c1
John Doe <klever1988@protonmail.com>
2023-12-07 12:49:06 +0800

    fix

--- scripts/merge_packages.sh
+++ scripts/merge_packages.sh
@@ -1,22 +1,10 @@
 function merge_package(){
-    pn=`echo $1 | rev | cut -d'/' -f 1 | rev`
-    find package/ -follow -name $pn -not -path "package/custom/*" | xargs -rt rm -rf
-    if [ ! -z "$2" ]; then
-        find package/ -follow -name $2 -not -path "package/custom/*" | xargs -rt rm -rf
-    fi
-
-    if [[ $1 == *'/trunk/'* || $1 == *'/branches/'* ]]; then
-        svn export $1
-    elif [[ $1 == *'/tree/'* ]]; then
-        repo=`echo $1|awk -F'/tree/' '{print $1}'`
-        git clone --depth=1 --single-branch $repo .tmprepo
-        mv .tmprepo/$pn $pn
-        rm .tmprepo -rf
-    else
-        git clone --depth=1 --single-branch $3 $1
-        rm -rf $pn/.git
-    fi
-    mv $pn package/custom/
+    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
+    find package/ -follow -name $3 -not -path "package/custom/*" | xargs -rt rm -rf
+    git clone --depth=1 --single-branch $1
+    mv $2 $3
+    rm -rf $repo
+    mv $3 package/custom/
 }
 function drop_package(){
     find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
@@ -36,21 +24,21 @@ merge_feed nas "https://github.com/linkease/nas-packages;master"
 merge_feed nas_luci "https://github.com/linkease/nas-packages-luci;main"
 rm -r package/feeds/nas_luci/luci-app-ddnsto
 merge_feed helloworld "https://github.com/stupidloud/helloworld;tmp"
-merge_package https://github.com/ilxp/luci-app-ikoolproxy
-merge_package https://github.com/sundaqiang/openwrt-packages/trunk/luci-app-wolplus
-merge_package https://github.com/messense/aliyundrive-webdav/trunk/openwrt/aliyundrive-webdav
-merge_package https://github.com/messense/aliyundrive-webdav/trunk/openwrt/luci-app-aliyundrive-webdav
-merge_package "-b 18.06 https://github.com/jerrykuku/luci-theme-argon"
-merge_package https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
-merge_package https://github.com/NateLol/luci-app-oled
-merge_package https://github.com/xiaorouji/openwrt-passwall-packages/tree/main/brook
-merge_package https://github.com/xiaorouji/openwrt-passwall-packages/tree/main/chinadns-ng
-merge_package https://github.com/xiaorouji/openwrt-passwall-packages/tree/main/trojan-go
-merge_package https://github.com/xiaorouji/openwrt-passwall-packages/tree/main/trojan-plus
-merge_package https://github.com/xiaorouji/openwrt-passwall-packages/tree/main/sing-box
-merge_package "-b main https://github.com/xiaorouji/openwrt-passwall"
-merge_package https://github.com/jerrykuku/lua-maxminddb
-merge_package https://github.com/kongfl888/luci-app-adguardhome
+merge_package https://github.com/ilxp/luci-app-ikoolproxy luci-app-ikoolproxy luci-app-ikoolproxy
+merge_package https://github.com/sundaqiang/openwrt-packages openwrt-packages/luci-app-wolplus luci-app-wolplus
+merge_package https://github.com/messense/aliyundrive-webdav aliyundrive-webdav/openwrt/aliyundrive-webdav aliyundrive-webdav
+merge_package https://github.com/messense/aliyundrive-webdav aliyundrive-webdav/openwrt/luci-app-aliyundrive-webdav luci-app-aliyundrive-webdav
+merge_package "-b 18.06 https://github.com/jerrykuku/luci-theme-argon" luci-theme-argon luci-theme-argon
+merge_package https://github.com/vernesong/OpenClash OpenClash/luci-app-openclash luci-app-openclash
+merge_package https://github.com/NateLol/luci-app-oled NateLol/luci-app-oled luci-app-oled
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/brook brook
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/chinadns-ng chinadns-ng
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/trojan-go trojan-go
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/trojan-plus trojan-plus
+merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/sing-box sing-box
+merge_package "-b main https://github.com/xiaorouji/openwrt-passwall" openwrt-passwall openwrt-passwall
+merge_package https://github.com/jerrykuku/lua-maxminddb lua-maxminddb lua-maxminddb
+merge_package https://github.com/kongfl888/luci-app-adguardhome luci-app-adguardhome luci-app-adguardhome
 drop_package luci-app-cd8021x
 drop_package luci-app-cifs
 drop_package verysync
